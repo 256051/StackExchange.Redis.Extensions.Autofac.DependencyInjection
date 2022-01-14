7ca467af29f0094b1a9f07ad76043ab4abab08b4
Hai <hai.yu@szymr.com>
2022-01-14 11:42:12 +0800

    update yml

--- publish-packages.sh
+++ publish-packages.sh
@@ -3,7 +3,7 @@ echo "package file"
 cd src
         dotnet pack *.csproj --include-symbols -c Release --output "."
         sleep 5
-        dotnet nuget push -s ${NUGET_API_URL} -k ${NUGET_KEY} "StackExchange.Redis.Extensions.Autofac.DependencyInjection.1.0.0.nupkg"
+        dotnet nuget push -s ${NUGET_API_URL} -k ${NUGET_KEY} "StackExchange.Redis.Extensions.Autofac.DependencyInjection.1.0.0.symbols.nupkg"
         echo "success"
         if [[ ${?} != 0  ]]
         then
