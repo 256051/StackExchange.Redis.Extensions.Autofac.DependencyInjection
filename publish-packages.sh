14a3cff37e37600385ab46c3d6f4822121442a39
Hai <hai.yu@szymr.com>
2022-01-14 11:56:24 +0800

    update yml

--- publish-packages.sh
+++ publish-packages.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 cd src
+echo "src"
 for directory in *; do 
+    echo ${directory}
     if [[ -d ${directory} ]]; then
         cd ${directory}
         dotnet pack *.csproj --include-symbols -c Release --output "."
         sleep 5
-        dotnet nuget push -s ${NUGET_API_URL} -k ${NUGET_KEY} "${directory}.1.0.0.symbols.nupkg"
+        dotnet nuget push -s ${NUGET_API_URL} -k ${NUGET_KEY} "${directory}.${TAG}.symbols.nupkg"
         echo "success"
         if [[ ${?} != 0  ]]
         then
